c95bb938df7d98bc8231a98e24b2aa41a7dc3d10
Sergio Romero Véliz <sergioromeroiv@gmail.com>
2025-05-21 02:26:58 -0400

    add: landing page and repo link

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,18 +5,35 @@
 </script>
 
 <nav class="navbar bg-base-100 flex justify-between p-4 shadow-sm">
-	<h1 class="btn btn-ghost text-3xl font-bold">GitOrb</h1>
+	<div class="flex items-center gap-5">
+		<a
+			href="https://github.com/AyanoT1/GitOrb"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="hover:text-primary ms-2 ml-auto flex items-center gap-2 transition-colors"
+			aria-label="View source on GitHub"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 24 24"
+				class="h-7 w-7"
+			>
+				<path
+					d="M12 0C5.37 0 0 5.373 0 12a12.002 12.002 0 008.21 11.387c.6.11.82-.26.82-.577v-2.234c-3.338.724-4.042-1.61-4.042-1.61-.546-1.386-1.334-1.756-1.334-1.756-1.09-.745.082-.73.082-.73 1.204.084 1.837 1.237 1.837 1.237 1.07 1.834 2.809 1.304 3.493.997.107-.775.42-1.304.762-1.603-2.665-.305-5.466-1.335-5.466-5.933 0-1.31.468-2.382 1.236-3.222-.124-.303-.535-1.527.117-3.183 0 0 1.008-.322 3.3 1.23a11.43 11.43 0 013.003-.403c1.02.005 2.047.137 3.003.403 2.29-1.552 3.295-1.23 3.295-1.23.654 1.656.243 2.88.12 3.183.77.84 1.235 1.913 1.235 3.222 0 4.61-2.805 5.625-5.476 5.922.431.372.815 1.105.815 2.228v3.303c0 .32.217.694.825.576A12.006 12.006 0 0024 12c0-6.627-5.373-12-12-12z"
+				/>
+			</svg>
+		</a>
+		<h1 class="text-3xl font-bold">GitOrb</h1>
+	</div>
 	<div class="me-2 flex items-center gap-4">
 		<ThemeSelector />
-		<div class="dropdown dropdown-end">
-			<div tabindex="0" role="button" class="rounded-field avatar avatar-online">
-				<div class="w-12 cursor-pointer rounded-full">
+		<div class="avatar avatar-online">
+			<div class="w-10 cursor-pointer rounded-full">
+				<a href={user.html_url} target="_blank">
 					<img src={user.avatar_url} alt="pfp" />
-				</div>
+				</a>
 			</div>
-			<ul class="menu dropdown-content bg-base-200 rounded-box z-1 mt-4 w-52 p-2 shadow-sm">
-				<li>Item 1</li>
-			</ul>
 		</div>
 	</div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="min-h-screen flex flex-col items-center justify-center bg-base-100 px-4 text-center">
+	<h1 class="text-4xl font-bold mb-4">Welcome to GitOrb!</h1>
+	<p class="text-lg text-base-content/70 mb-6">
+		Orchestrate your workflow with this no bs GitHub dashboard!.
+	</p>
+	<a class="btn btn-primary" href="/dashboard">Go to Dashboard</a>
+</section>
+
