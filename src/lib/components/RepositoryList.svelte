dd501057cf855e84290313264e8e834b2a463b7b
Sergio Romero Véliz <sergioromeroiv@gmail.com>
2025-05-21 14:26:16 -0400

    add: private repo indicator

--- src/lib/components/RepositoryList.svelte
+++ src/lib/components/RepositoryList.svelte
@@ -10,6 +10,7 @@
 		html_url: string;
 		description: string | undefined;
 		updated_at: string;
+		private: boolean;
 	}
 
 	interface Props {
@@ -50,9 +51,30 @@
 							{/if}
 						</section>
 
-						<section class="shrink-0 text-right whitespace-nowrap">
-							<p class="text-sm text-gray-500">Last updated:</p>
-							<p class="text-xs text-gray-500">{formatDate(repo.updated_at)}</p>
+						<section
+							class="flex shrink-0 flex-col items-end gap-1 text-right whitespace-nowrap text-gray-500"
+						>
+							{#if repo.private}
+								<div class="flex items-center gap-1 text-xs">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										stroke-width="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										class="h-4 w-4"
+									>
+										<circle cx="12" cy="16" r="1" />
+										<rect x="3" y="10" width="18" height="12" rx="2" />
+										<path d="M7 10V7a5 5 0 0 1 10 0v3" />
+									</svg>
+									<span>Private</span>
+								</div>
+							{/if}
+							<p class="text-sm">Last updated:</p>
+							<p class="text-xs">{formatDate(repo.updated_at)}</p>
 						</section>
 					</main>
 				</div>
