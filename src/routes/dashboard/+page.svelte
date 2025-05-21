5cc482b1dde2f2bdf2067dd740a12f1726d89bcd
Sergio Romero Véliz <sergioromeroiv@gmail.com>
2025-05-20 20:52:53 -0400

    add: basic navbar with user context

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,13 +1,18 @@
 <script>
+	import Navbar from '$lib/components/Navbar.svelte';
+
 	export let data;
 </script>
 
-<h2 class="text-lg font-semibold mb-2">Repositories</h2>
+<Navbar user={data.user}></Navbar>
+
+<h2 class="mb-2 text-lg font-semibold">Repositories</h2>
+
 
 <ul class="space-y-2">
 	{#each data.repos as repo}
 		<li>
-			<a href={repo.html_url} target="_blank" class="underline text-blue-500">
+			<a href={repo.html_url} target="_blank" class="text-blue-500 underline">
 				{repo.full_name}
 			</a>
 		</li>
